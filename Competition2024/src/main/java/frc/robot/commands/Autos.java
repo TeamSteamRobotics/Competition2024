802a152d76c2b7d9aa2f4cdaa89047c8f3c484db
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2024-01-20 13:04:49 -0600

    sejun work

--- Competition2024/src/main/java/frc/robot/commands/Autos.java
+++ Competition2024/src/main/java/frc/robot/commands/Autos.java
@@ -5,10 +5,37 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
+  private Trajectory m_trajectory; 
+  private Field2d m_field;
+  public void robotInit()  {
+    m_trajectory = 
+      TrajectoryGenerator.generateTrajectory(
+      new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+      List.of(new Translation2d(3, 3), new Translation2d(-3, -3)),
+      new Pose2d(0,0, Rotation2d.fromDegrees(0)),
+      new TrajectoryConfig(Units.feetToMeters(3.0), Units.feetToMeters(3.0)));
+// This stuff determines the path the robot takes and keeps track of all the translations
+      m_field = new Field2d();
+      SmartDashboard.putData(m_field);
+
+      m_field.getObject("traj").setTrajectory(m_trajectory);
+  }
   /** Example static factory for an autonomous command. */
   public static Command exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
--- Competition2024/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
+++ Competition2024/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
@@ -4,26 +4,62 @@
 
 package frc.robot.subsystems;
 
+import java.util.Scanner;
+
 import javax.security.auth.x500.X500Principal;
 
+import org.opencv.core.Point;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SmartDashboardSubsystem extends SubsystemBase {
+  private final Field2d m_field = new Field2d();
+
+  
   /** Creates a new SmartDashboardSubsystem. */
   public SmartDashboardSubsystem() {
     SmartDashboard.putNumber("X Position in inches", 0);
     SmartDashboard.putNumber("Y Position in inches",0);
     SmartDashboard.putNumber("Rotation in degrees",0);
     SmartDashboard.putStringArray("Coordinate Lists", new String[2]);
+    SmartDashboard.putString("Point", "S1");
+    SmartDashboard.putData("Field", m_field);
   }
 
   @Override
-  public void periodic() {
-     
+  public void periodic(){
+  // String input = SmartDashboard.getString("Point", "S1");
+  
+  //   if (input == "S1"){}
+  //   else if (input == "A1"){}
+  //   else if (input == "S2"){}
+  //   else if (input == "S3"){}
+  //   else if (input == "G1"){}
+  //   else if (input == "G2"){}
+
+  //Else if standard system
     double X = SmartDashboard.getNumber("X Position in inches", 0);
     SmartDashboard.getNumber("Y Position",0);
     SmartDashboard.putNumber("Test", X);
     // This method will be called once per scheduler run
+
+    m_field.setRobotPose(new Pose2d(1, 1, new Rotation2d(0) )); 
+    // fix the values later to stay current (got errors we didnt know how to fix)
   }
+
+  
+  private Pose2d S1 = new Pose2d(5, 5, new Rotation2d(39));
+  private Pose2d A1 = new Pose2d(5, 5, new Rotation2d(39));
+  private Pose2d S2 = new Pose2d(5, 5, new Rotation2d(39));
+  private Pose2d S3 = new Pose2d(5, 5, new Rotation2d(39));
+  private Pose2d G2 = new Pose2d(5, 5, new Rotation2d(39));
+  private Pose2d G1 = new Pose2d(5, 5, new Rotation2d(39));
+  // All the points, change later
+  
+
+  
 }
