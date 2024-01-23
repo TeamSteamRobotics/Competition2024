23908ae2a05d0d1005a80c67aa5672eb533490bc
Zachary Hansen Terry <Zmht25@gmail.com>
2024-01-22 20:15:33 -0600

    created stuff for running from input to output of auto stuff

--- Competition2024/src/main/java/frc/robot/AutoPoints.java
+++ Competition2024/src/main/java/frc/robot/AutoPoints.java
@@ -3,7 +3,7 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class AutoPoints {
-
+    // 0 in the first dimension means blue!
     public static final Pose2d[][] bArray = new Pose2d[2][5];
     public static final Pose2d[][] nArray = new Pose2d[2][7];
     public static final Pose2d[][] sArray = new Pose2d[2][4];
--- Competition2024/src/main/java/frc/robot/commands/MainAutoCommand.java
+++ Competition2024/src/main/java/frc/robot/commands/MainAutoCommand.java
@@ -4,27 +4,66 @@
 
 package frc.robot.commands;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.StringParsing;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class MainAutoCommand extends Command {
   /** Creates a new MainAutoCommand. */
-  public MainAutoCommand() {
+  private String inputString;
+  private String[] formattedString;
+  private ArrayList<Pose2d> positionList;
+  private Optional<Alliance> alliance;
+  private boolean isBlue;
+  private DriveSubsystem driveSubsystem;
+
+  public MainAutoCommand(String p_inputString, DriveSubsystem p_driveSubsystem) {
+    inputString = p_inputString;
+    driveSubsystem = p_driveSubsystem;
+    positionList = new ArrayList<Pose2d>();
+    alliance = DriverStation.getAlliance();
+    addRequirements(driveSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void initialize() {
+    if(alliance.isPresent()) {
+      if(alliance.get() == Alliance.Blue) {
+        isBlue = true;
+      }
+      else if(alliance.get() == Alliance.Red) {
+        isBlue = false;
+      }
+    }
+    else {
+      isBlue = true; // 50-50 chance that we right if we cant get the correct info so we take the odds better than doing nothing right???
+    }
 
-  // Called once the command ends or is interrupted.
+    formattedString = StringParsing.parsePointList(inputString);
+    for(String point : formattedString) {
+      positionList.add(StringParsing.parseInput(point, isBlue));
+    }
+  }
+
+  @Override
+  public void execute() {
+    for(Pose2d position : positionList) {
+      new GoToPoint(driveSubsystem, position);
+    }
+
+  }
+
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
