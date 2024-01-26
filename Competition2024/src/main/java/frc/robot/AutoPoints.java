3a9fc167404ab01d2000c0f027c35d6a22189f6a
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2024-01-25 20:57:12 -0600

    added enum

--- Competition2024/src/main/java/frc/robot/AutoPoints.java
+++ Competition2024/src/main/java/frc/robot/AutoPoints.java
@@ -1,15 +1,21 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
-
+import edu.wpi.first.math.geometry.Rotation2d;
+enum allianceColor{
+        BLUE,
+        RED
+ }
 public class AutoPoints {
     // 0 in the first dimension means blue!
 
     public static final Pose2d[][] bArray = new Pose2d[2][5];
     public static final Pose2d[][] nArray = new Pose2d[2][7];
     public static final Pose2d[][] sArray = new Pose2d[2][4];
+    public AutoPoints() {
+        bArray[0][2] = new Pose2d(0, 0, new Rotation2d());
+    }
 
-    
+    allianceColor currentAlliance = allianceColor.BLUE;
 }
 
- 
