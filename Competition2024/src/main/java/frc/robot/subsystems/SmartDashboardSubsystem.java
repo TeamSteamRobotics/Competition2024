53bd9ef0063decee3aef0d5787fb48bd80b59046
LogEman <thelogankessler@gmail.com>
2024-01-27 11:50:49 -0600

    Added Apriltags to SmartDash
    All 16 apriltags will now have their x, y, z, rx, ry, and rz displayed in the smart dashboard.

--- Competition2024/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
+++ Competition2024/src/main/java/frc/robot/subsystems/SmartDashboardSubsystem.java
@@ -10,10 +10,12 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.AprilVisionSubsystem;
+import frc.robot.subsystems.AprilVisionSubsystem.ReturnTarget;
 
 public class SmartDashboardSubsystem extends SubsystemBase {
   private final Field2d m_field = new Field2d();
-
+  AprilVisionSubsystem av;
   
   /** Creates a new SmartDashboardSubsystem. */
   public SmartDashboardSubsystem() {
@@ -37,7 +39,24 @@ public class SmartDashboardSubsystem extends SubsystemBase {
     SmartDashboard.getNumber("Y Position",0);
     SmartDashboard.putNumber("Test", X);
     // This method will be called once per scheduler run
-
+    for(int atCount = 1; atCount <= 16; atCount++){
+      if(av.getCoordinates(atCount, ReturnTarget.TARGET).aprilTagVisible){
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " X: "), av.getCoordinates(atCount, ReturnTarget.TARGET).x);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " Y: "), av.getCoordinates(atCount, ReturnTarget.TARGET).y);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " Z: "), av.getCoordinates(atCount, ReturnTarget.TARGET).z);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RX: "), av.getCoordinates(atCount, ReturnTarget.TARGET).rx);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RY: "), av.getCoordinates(atCount, ReturnTarget.TARGET).ry);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RZ: "), av.getCoordinates(atCount, ReturnTarget.TARGET).rz);
+      }else{
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " X: "), -1000);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " Y: "), -1000);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " Z: "), -1000);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RX: "), -1000);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RY: "), -1000);
+          SmartDashboard.putNumber(("Apriltag ID " + atCount + " RZ: "), -1000);
+      }
+    }
+    
     m_field.setRobotPose(new Pose2d(1, 1, new Rotation2d(0) )); 
     // fix the values later to stay current (got errors we didnt know how to fix)
   }
