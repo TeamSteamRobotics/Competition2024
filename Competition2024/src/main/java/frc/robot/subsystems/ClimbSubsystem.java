c9994fb8db98ad95a174f8655ee4f57ef35c56be
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2024-03-07 21:33:15 -0600

    update

--- Competition2024/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ Competition2024/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -22,7 +22,7 @@ public class ClimbSubsystem extends SubsystemBase {
 
   private RelativeEncoder rightEncoder;
 
-  private double climbSpeed = 0.8;
+  private double climbSpeed = 1.0;
   
   public ClimbSubsystem() {
     leftClimb = new CANSparkMax(CANID.leftClimb, MotorType.kBrushless);
