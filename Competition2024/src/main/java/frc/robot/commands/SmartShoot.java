285dc2ed78c80c371122963717a34250b2968874
LogEman <thelogankessler@gmail.com>
2024-03-12 00:46:31 -0500

    Offset, Dual-side functionality.
    - Added offset variables for both the z, and x axis.
    
    - Added if statements to check whether the blue apriltag is visible or the red apriltag is visible; with it using the apriltag value that is visible.

--- Competition2024/src/main/java/frc/robot/commands/SmartShoot.java
+++ Competition2024/src/main/java/frc/robot/commands/SmartShoot.java
@@ -16,32 +16,55 @@ import frc.robot.subsystems.AprilVisionSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.AprilVisionSubsystem.ReturnTarget;
 
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class SmartShoot extends ParallelRaceGroup {
   /** Creates a new SmartShoot. */
-  private boolean notVisibleEnd = false;
+  //private boolean notVisibleEnd = false;
+
+
+  //DEFINE APRILTAG ID FOR EACH ALLIANCE
+  private int blueAprilTagId = 8;
+  private int redAprilTagId = 4;
+
+  //OFFSETS FOR APRILTAGS
+  private double zOffset = 0.0;
+  private double xOffset = 0.0;
+
   private double shooterSpeedRPM, shooterAngleDegrees;
   public SmartShoot(ShooterSubsystem shoot, AprilVisionSubsystem aprilVision) { 
-    
     //shooterAngleDegrees = shoot.getTargetAngle(aprilVision.getCoordinates(4, ReturnTarget.TARGET).z);
     shooterSpeedRPM = 1500; 
     // addCommands(new FooCommand(), new BarCommand());
-    /*if(aprilVision.getCoordinates(4, ReturnTarget.TARGET).aprilTagVisible){
-      notVisibleEnd = false;*/
+
+    //Checks which apriltag is visible, that being the one belonging to the blue alliance or the one belonging to the red alliance, and uses that for smartshoot calulations/command scheduling.
+    if(aprilVision.getCoordinates(blueAprilTagId, ReturnTarget.TARGET).aprilTagVisible){
     addCommands(
       new ShootPID(shoot, shooterSpeedRPM),
       new AngleShooterPID(shoot, () -> shoot.getTargetAngle(
+        //Offsets applied in calculations.
         Math.sqrt(
-          Math.pow(aprilVision.getCoordinates(4, ReturnTarget.TARGET).z, 2) +
-          Math.pow(aprilVision.getCoordinates(4, ReturnTarget.TARGET).x, 2)
+          Math.pow(aprilVision.getCoordinates(blueAprilTagId, ReturnTarget.TARGET).z + zOffset, 2) +
+          Math.pow(aprilVision.getCoordinates(blueAprilTagId, ReturnTarget.TARGET).x + xOffset, 2)
         ))));
-   /* }else{
+    }else if(aprilVision.getCoordinates(redAprilTagId, ReturnTarget.TARGET).aprilTagVisible){
+    addCommands(
+      new ShootPID(shoot, shooterSpeedRPM),
+      new AngleShooterPID(shoot, () -> shoot.getTargetAngle(
+        //Offsets applied in calculations.
+        Math.sqrt(
+          Math.pow(aprilVision.getCoordinates(redAprilTagId, ReturnTarget.TARGET).z + zOffset, 2) +
+          Math.pow(aprilVision.getCoordinates(redAprilTagId, ReturnTarget.TARGET).x + xOffset, 2)
+        ))));
+  
+    //Safety net for if neither apriltag is visible.
+    }else{
       addCommands(new InstantCommand(() -> System.out.println("APRILTAG NOT VISIBLE. COMMAND NOT RUN!!!!")));
-      notVisibleEnd = true;
+      //notVisibleEnd = true;
       System.out.println("APRILTAG IS NOT VISIBLE!!");
-    }*/
-     // new WaitCommand(5));
+    }
+     new WaitCommand(5);
   }
 }
